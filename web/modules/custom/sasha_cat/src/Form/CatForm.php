c3a1d56160f107b97325415578f6caf48468af3f
sasha <saschapanasyuk27@gmail.com>
2022-02-18 10:19:31 +0200

    task-6 commit

--- web/modules/custom/sasha_cat/src/Form/CatForm.php
+++ web/modules/custom/sasha_cat/src/Form/CatForm.php
@@ -60,7 +60,7 @@ class CatForm extends FormBase
     $response = new AjaxResponse();
     if ($form_state->hasAnyErrors()) {
       foreach ($form_state->getErrors() as $errors_array) {
-        $response->addCommand(new MessageCommand($errors_array));
+        $response->addCommand(new MessageCommand($errors_array, NULL, ['type' => 'warning']));
       }
     } else {
       $response->addCommand(new MessageCommand('Congratulations! You added your cat!'));
