142085e1b7aa4a27b346f11b7af86fccce2083da
sasha <saschapanasyuk27@gmail.com>
2022-02-23 17:19:50 +0200

    task-10 commit

--- web/modules/custom/sasha_cat/src/Controller/SashaCatController.php
+++ web/modules/custom/sasha_cat/src/Controller/SashaCatController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Drupal\sasha_cat\Controller;
-
+use Drupal\file\Entity\File;
 use Drupal\Core\Controller\ControllerBase;
 
 /**
@@ -26,8 +26,38 @@ class SashaCatController extends ControllerBase {
     return [
       '#theme' => 'sasha_cat',
       $form,
+      '#list'=>$this->catTable(),
     ];
 
+  }
+  public function catTable(): array
+  {
+    $query= \Drupal::database();
+    $result = $query->select('sasha_cat', 'sasha_cattb')
+      ->fields('sasha_cattb', ['name', 'email', 'image', 'date'])
+      ->orderBy('date', 'DESC')
+      ->execute()->fetchAll();
+    $data = [];
+    foreach ($result as $row) {
+      $file = File::load($row->image);
+      $uri = $file->getFileUri();
+      $photoCats = [
+        '#theme' => 'image',
+        '#uri' => $uri,
+        '#alt' => 'cat',
+        '#title' => 'cat',
+        '#width' => 255,
+      ];
+      $data[] = [
+        'name' => $row->name,
+        'email' => $row->email,
+        'image' => [
+          'data' => $photoCats,
+        ],
+        'date' => $row->date,
+      ];
+    };
+    return $data;
   }
 
 
--- web/modules/custom/sasha_cat/src/Form/CatForm.php
+++ web/modules/custom/sasha_cat/src/Form/CatForm.php
@@ -6,6 +6,9 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\MessageCommand;
+use Drupal\Core\Url;
+use Drupal\file\Entity\File;
+use Drupal\Core\Database\Connection;
 
 class CatForm extends FormBase
 {
@@ -24,6 +27,7 @@ class CatForm extends FormBase
       '#title' => $this->t('Your cat’s name:'),
       '#placeholder' => $this->t('The name must be in range from 2 to 32 symbols'),
       '#required' => TRUE,
+      '#maxlength' => 32,
     ];
     $form['email'] = [
       '#type' => 'email',
@@ -65,37 +69,69 @@ class CatForm extends FormBase
 
   public function validateForm(array &$form, FormStateInterface $form_state)
   {
+    if(!$this->validateName($form, $form_state)){
+      return false;
+    }
+  }
+  public function validateName(array &$form, FormStateInterface $form_state)
+  {
     if ((mb_strlen($form_state->getValue('adding_cat')) < 2)) {
-      $form_state->setErrorByName('adding_cat', $this->t('Your cat`s name should be more than 2 symbols.'));
+      return false;
+    } elseif ((mb_strlen($form_state->getValue('adding_cat')) > 32)) {
+      return false;
     }
-    if ((mb_strlen($form_state->getValue('adding_cat')) > 32)) {
-      $form_state->setErrorByName('adding_cat', $this->t('Your cat`s name should be less than 32 symbols.'));
-    }
+    return true;
   }
 
 
+  public function validateEmail(array &$form, FormStateInterface $form_state)
+  {
+    if (!preg_match("/^[a-zA-Z_\-]+@[a-zA-Z_\-\.]+\.[a-zA-Z\.]{2,6}+$/", $form_state->getValue('email'))) {
+      $form_state->setErrorByName('email', $this->t('Your email is NOT invalid'));
+      return false;
+    }
+    return true;
+  }
+
   public function submitForm(array &$form, FormStateInterface $form_state)
   {
-//    \Drupal::messenger()->addStatus(t('Congratulations! You added your cat!;)'));
+    if($this->validateName($form, $form_state)){
+      $picture = $form_state->getValue('cat_image');
+      $file = File::load($picture[0]);
+      $file->setPermanent();
+      $file->save();
+
+      $cat = [
+        'name' => $form_state->getValue('adding_cat'),
+        'email' => $form_state->getValue('email'),
+        'image' => $picture[0],
+        'date' => date('d-m-Y H:i:s', strtotime('+3 hour')),
+      ];
+
+      \Drupal::database()->insert('sasha_cat')->fields($cat)->execute();
+    }
+
   }
 
   public function AjaxSubmit(array &$form, FormStateInterface $form_state)
   {
     $response = new AjaxResponse();
-    if ($form_state->hasAnyErrors()) {
-      foreach ($form_state->getErrors() as $errors_array) {
-        $response->addCommand(new MessageCommand($errors_array, NULL, ['type' => 'error']));
-      }
+    $nameValid = $this->validateName($form, $form_state);
+
+    if (!$nameValid) {
+      $response->addCommand(new MessageCommand('Your name is NOT valid', ".null", ['type' => 'error'],));
     } else {
       $response->addCommand(new MessageCommand('Congratulations! You added your cat!'));
     }
     \Drupal::messenger()->deleteAll();
+    $url = Url::fromRoute('sasha.cats');
     return $response;
   }
+
   public function AjaxEmail(array &$form, FormStateInterface $form_state)
   {
     $response = new AjaxResponse();
-    if (preg_match("/^[a-zA-Z_\-]+@[a-zA-Z_\-\.]+\.[a-zA-Z\.]{2,6}+$/", $form_state->getValue('email'))){
+    if (preg_match("/^[a-zA-Z_\-]+@[a-zA-Z_\-\.]+\.[a-zA-Z\.]{2,6}+$/", $form_state->getValue('email'))) {
       $response->addCommand(new MessageCommand('Your email is valid'));
     } else {
       $response->addCommand(new MessageCommand('Your email is NOT valid', ".null", ['type' => 'error'], ));
